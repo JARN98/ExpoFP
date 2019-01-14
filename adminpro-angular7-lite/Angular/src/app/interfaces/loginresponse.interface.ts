9930bad0551010f073abbd06938f4e40058e636d
BLQZ <alejandro22blazquez06@gmail.com>
2019-01-14 10:22:46 +0100

    L14-Mas vale libreria en mano que ciento volando

---
+++ adminpro-angular7-lite/Angular/src/app/interfaces/loginresponse.interface.ts
@@ -0,0 +1,6 @@
+export interface LoginResponse {
+    token: string;
+    nombre: string;
+    email: string;
+    role: string;
+}
---
+++ adminpro-angular7-lite/Angular/src/app/services/session.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { LoginDto } from '../dto/login.dto';
+import { Observable } from 'rxjs';
+import { LoginResponse } from '../interfaces/loginresponse.interface';
+
+const authUrl = `http://localhost:9000/auth`;
+
+const requestOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService {
+
+  constructor(private http: HttpClient) { }
+
+  login(loginDto: LoginDto): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${authUrl}/login`, loginDto, requestOptions);
+  }
+
+  setLoginData(loginResponse: LoginResponse) {
+    localStorage.setItem('token', loginResponse.token);
+    localStorage.setItem('nombre', loginResponse.nombre);
+    localStorage.setItem('email', loginResponse.email);
+    localStorage.setItem('rol', loginResponse.role);
+  }
+}
