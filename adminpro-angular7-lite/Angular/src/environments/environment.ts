b157af94072e4a2c8c26fbb31058d18c95a538cd
JARN98 <jose.arn73@gmail.com>
2019-01-17 21:04:31 +0100

    moraleja: Más malo que los delanteros del betis...

--- adminpro-angular7-lite/Angular/src/environments/environment.ts
+++ adminpro-angular7-lite/Angular/src/environments/environment.ts
@@ -3,7 +3,19 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+
+const firebase = require('firebase');
 export const environment = {
   production: false,
-  ApiUrl: 'http://localhost:9000'
+  ApiUrl: 'http://localhost:9000',
+  firebase: {
+    apiKey: 'AIzaSyBDXQQGWcrjWJw2GCsf2AzUKx9fpwBSRAg',
+    authDomain: 'expofp-1547741688653.firebaseapp.com',
+    databaseURL: 'https://expofp-1547741688653.firebaseio.com',
+    projectId: 'expofp-1547741688653',
+    storageBucket: 'expofp-1547741688653.appspot.com',
+    messagingSenderId: '841299973118'
+  }
 };
+
+export const  storage = firebase.app().storage('gs://expofp-1547741688653.appspot.com');
