392ba2778abfe1a924934a1556c0da488fe02955
unknown <cristobal.1dam@gmail.com>
2019-01-17 21:57:47 +0100

    Navbar casi terminado, falta el perfil desplegable moraleja: tu ta tutu tuta pa lo bueno y pa lo malo

---
+++ adminpro-angular7-lite/Angular/src/app/shared/header-navigation/menu-items.ts
@@ -0,0 +1,26 @@
+import { RouteInfo } from './topnav.metadata';
+
+export const ROUTES: RouteInfo[] = [
+ 
+  {
+    path: '/component/proyectos',
+    title: 'Proyectos',
+    icon: 'mdi mdi-white-balance-incandescent',
+    class: '',
+    label: '',
+    labelClass: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/encuesta',
+    title: 'Encuesta',
+    icon: 'mdi mdi-clipboard-outline',
+    class: '',
+    label: '',
+    labelClass: '',
+    extralink: false,
+    submenu: []
+  },
+ 
+];
