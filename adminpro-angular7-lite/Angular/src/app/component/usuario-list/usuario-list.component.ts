6d6a047c1c0560936730340e174f2b8753c9b01f
jcampos <juancampe96@gmail.com>
2019-01-21 13:31:24 +0100

    listar usuarios arreglado, Moraleja: Cada maestrillo tiene su librillo

--- adminpro-angular7-lite/Angular/src/app/component/usuario-list/usuario-list.component.ts
+++ adminpro-angular7-lite/Angular/src/app/component/usuario-list/usuario-list.component.ts
@@ -4,6 +4,8 @@ import { ListUsuarioService } from '../../services/list-usuario.service';
 import { AuthService } from '../../services/auth.service';
 import { MatSnackBar, MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
+import { ListApiResponse } from '../../interfaces/list-api.interface';
+import { ListUsuariosResponse } from '../../interfaces/list-usuarios';
 
 
 @Component({
@@ -12,31 +14,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./usuario-list.component.scss']
 })
 export class UsuarioListComponent implements OnInit {
-
+  listaApi: ListApiResponse;
+  listaUsuariosRes: ListUsuariosResponse[];
   displayedColumns: string[] = ['userId', 'username', 'email'];
-  dataSource: User[];
+  dataSource: ListUsuariosResponse[];
 
   constructor(private listUsuarioService: ListUsuarioService, private authService: AuthService,
               public snackBar: MatSnackBar, public dialog: MatDialog, private router: Router) { }
 
   ngOnInit() {
-    this.listUser('Lista de usuarios obtenidas correctamente');
+    this.listUser();
   }
 
-  listUser(mensaje: string) {
-    this.listUsuarioService.listUser().subscribe(listUser => {
-      this.dataSource = listUser;
-      this.snackBar.open(mensaje, 'Cerrar', {
-        duration: 3000,
-        verticalPosition: 'top'
-      });
-    }, error =>  {
-      // this.snackBar.open('Error al obtener usuarios', 'Cerrar', {
-      //   duration: 3000,
-      // });
+  listUser() {
+    this.listUsuarioService.listUsuarios().subscribe(lista => {
+      this.listaApi = lista;
+      this.listaUsuariosRes = this.listaApi.rows;
+      // this.dataSource = lista;
+      console.log(this.listaUsuariosRes);
+
+    }, error => {
+      console.error(error);
     });
-
-
   }
 
   /*
