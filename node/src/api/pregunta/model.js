7254c6cc2a26ac510c3d88f8c27c730fadce551e
BLQZ <alejandro22blazquez06@gmail.com>
2019-01-29 16:08:33 +0100

    Documento Pregunta editado añadiendo atributos nA, nB, nC

--- node/src/api/pregunta/model.js
+++ node/src/api/pregunta/model.js
@@ -1,44 +1,56 @@
 import mongoose, { Schema } from 'mongoose'
 
 const preguntaSchema = new Schema({
-  pregunta: {
-    type: String
-  },
-  respuestaA: {
-    type: String
-  },
-  respuestaB: {
-    type: String
-  },
-  respuestaC: {
-    type: String
-  }
+    pregunta: {
+        type: String
+    },
+    respuestaA: {
+        type: String
+    },
+    respuestaB: {
+        type: String
+    },
+    respuestaC: {
+        type: String
+    },
+    nA: {
+        type: Number
+    },
+    nB: {
+        type: Number
+    },
+    nC: {
+        type: Number
+    }
 }, {
-  timestamps: true,
-  toJSON: {
-    virtuals: true,
-    transform: (obj, ret) => { delete ret._id }
-  }
+    timestamps: true,
+    toJSON: {
+        virtuals: true,
+        transform: (obj, ret) => { delete ret._id }
+    }
 })
 
 preguntaSchema.methods = {
-  view (full) {
-    const view = {
-      // simple view
-      id: this.id,
-      pregunta: this.pregunta,
-      respuestaA: this.respuestaA,
-      respuestaB: this.respuestaB,
-      respuestaC: this.respuestaC,
-      createdAt: this.createdAt,
-      updatedAt: this.updatedAt
-    }
+    view(full) {
+        const view = {
+            // simple view
+            id: this.id,
+            pregunta: this.pregunta,
+            respuestaA: this.respuestaA,
+            respuestaB: this.respuestaB,
+            respuestaC: this.respuestaC,
+            nA: this.nA,
+            nB: this.nB,
+            nC: this.nC,
+            createdAt: this.createdAt,
+            updatedAt: this.updatedAt
+        }
 
-    return full ? {
-      ...view
-      // add properties for a full view
-    } : view
-  }
+        return full ? {
+            ...view
+            // add properties for a full view
+        } : view
+    }
 }
 
 const model = mongoose.model('Pregunta', preguntaSchema)
