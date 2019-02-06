371aadf2fd329896a701fcea46e686b5c30e1e72
jcampos <juancampe96@gmail.com>
2019-02-06 08:30:36 +0100

    solucionado error en la importacion de modulos (ahora de verdad), Moraleja: si la vida te da la espalda, cogele el culo

--- adminpro-angular7-lite/Angular/src/app/component/component.module.ts
+++ adminpro-angular7-lite/Angular/src/app/component/component.module.ts
@@ -7,21 +7,6 @@ import { JsonpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ComponentsRoutes } from './component.routing';
-import { NgbdpregressbarBasicComponent } from './progressbar/progressbar.component';
-import { NgbdpaginationBasicComponent } from './pagination/pagination.component';
-import { NgbdAccordionBasicComponent } from './accordion/accordion.component';
-import { NgbdAlertBasicComponent } from './alert/alert.component';
-import { NgbdCarouselBasicComponent } from './carousel/carousel.component';
-import { NgbdDatepickerBasicComponent } from './datepicker/datepicker.component';
-import { NgbdDropdownBasicComponent } from './dropdown-collapse/dropdown-collapse.component';
-import { NgbdModalBasicComponent } from './modal/modal.component';
-import { NgbdPopTooltipComponent } from './popover-tooltip/popover-tooltip.component';
-import { NgbdratingBasicComponent } from './rating/rating.component';
-import { NgbdtabsBasicComponent } from './tabs/tabs.component';
-import { NgbdtimepickerBasicComponent } from './timepicker/timepicker.component';
-import { NgbdtypeheadBasicComponent } from './typehead/typehead.component';
-import { CardsComponent } from './card/card.component';
-import { ButtonsComponent } from './buttons/buttons.component';
 import { UsuarioListComponent } from './usuario-list/usuario-list.component';
 import { ListaProyectosComponent } from './lista-proyectos/lista-proyectos.component';
 import { MatTableModule } from '@angular/material/table';
@@ -57,21 +42,7 @@ import { MatRadioModule, MatButtonModule } from '@angular/material';
 
   ],
   declarations: [
-    NgbdpregressbarBasicComponent,
-    NgbdpaginationBasicComponent,
-    NgbdAccordionBasicComponent,
-    NgbdAlertBasicComponent,
-    NgbdCarouselBasicComponent,
-    NgbdDatepickerBasicComponent,
-    NgbdDropdownBasicComponent,
-    NgbdModalBasicComponent,
-    NgbdPopTooltipComponent,
-    NgbdratingBasicComponent,
-    NgbdtabsBasicComponent,
-    NgbdtimepickerBasicComponent,
-    NgbdtypeheadBasicComponent,
-    CardsComponent,
-    ButtonsComponent,
+
     UsuarioListComponent,
     ListaProyectosComponent,
     ProyectoDetalladoComponent,
