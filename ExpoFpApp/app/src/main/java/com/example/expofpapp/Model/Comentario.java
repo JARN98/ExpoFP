71d40c2c3745d895a94632a9be00a90e833ef5ba
jcampos <juancampe96@gmail.com>
2019-02-12 12:49:40 +0100

    implementado el imagePager en android con datos estaticos, moraleja: a melon bueno y maduro todos le hueles el culo

---
+++ ExpoFpApp/app/src/main/java/com/example/expofpapp/Model/Comentario.java
@@ -0,0 +1,83 @@
+package com.example.expofpapp.Model;
+
+public class Comentario {
+
+    private String nombreAutor;
+    private String contenido;
+    private int valoracion;
+    private boolean valido;
+    private String imagenAutor;
+    private String proyecto;
+
+    public Comentario() {
+    }
+
+    public Comentario(String nombreAutor, String contenido, int valoracion, boolean valido, String imagenAutor, String proyecto) {
+        this.nombreAutor = nombreAutor;
+        this.contenido = contenido;
+        this.valoracion = valoracion;
+        this.valido = valido;
+        this.imagenAutor = imagenAutor;
+        this.proyecto = proyecto;
+    }
+
+    public String getNombreAutor() {
+        return nombreAutor;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public int getValoracion() {
+        return valoracion;
+    }
+
+    public boolean isValido() {
+        return valido;
+    }
+
+    public String getImagenAutor() {
+        return imagenAutor;
+    }
+
+    public String getProyecto() {
+        return proyecto;
+    }
+
+    public void setNombreAutor(String nombreAutor) {
+        this.nombreAutor = nombreAutor;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public void setValoracion(int valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public void setValido(boolean valido) {
+        this.valido = valido;
+    }
+
+    public void setImagenAutor(String imagenAutor) {
+        this.imagenAutor = imagenAutor;
+    }
+
+    public void setProyecto(String proyecto) {
+        this.proyecto = proyecto;
+    }
+
+    @Override
+    public String toString() {
+        return "Comentario{" +
+                "nombreAutor='" + nombreAutor + '\'' +
+                ", contenido='" + contenido + '\'' +
+                ", valoracion=" + valoracion +
+                ", valido=" + valido +
+                ", imagenAutor='" + imagenAutor + '\'' +
+                ", proyecto='" + proyecto + '\'' +
+                '}';
+    }
+}
