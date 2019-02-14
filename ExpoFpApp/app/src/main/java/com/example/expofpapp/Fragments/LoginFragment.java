17d4fdd7e6dfa523d08cdf1d17d84975892a7619
JARN98 <jose.arn73@gmail.com>
2019-02-14 09:28:46 +0100

    Noo

--- ExpoFpApp/app/src/main/java/com/example/expofpapp/Fragments/LoginFragment.java
+++ ExpoFpApp/app/src/main/java/com/example/expofpapp/Fragments/LoginFragment.java
@@ -3,6 +3,8 @@ package com.example.expofpapp.Fragments;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,16 +16,40 @@ import android.widget.Toast;
 
 import com.example.expofpapp.Generator.UtilUser;
 import com.example.expofpapp.MainActivity;
+import com.example.expofpapp.Model.User;
+import com.example.expofpapp.Model.UserDto;
 import com.example.expofpapp.R;
 
 import com.example.expofpapp.Generator.ServiceGenerator;
 import com.example.expofpapp.Generator.UtilToken;
 import com.example.expofpapp.Model.LoginResponse;
 import com.example.expofpapp.Services.AuthService;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.concurrent.Executor;
+
 import okhttp3.Credentials;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
+import static android.support.constraint.Constraints.TAG;
 
 
 /**
@@ -34,11 +60,12 @@ import retrofit2.Response;
  * Use the {@link LoginFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final int RC_SIGN_IN = 777;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -82,7 +109,9 @@ public class LoginFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -104,6 +133,9 @@ public class LoginFragment extends Fragment {
                 mListener.navegarRegistro();
             }
         });
+
+
+
         return view;
     }
 
@@ -161,9 +193,19 @@ public class LoginFragment extends Fragment {
 
             }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
 
     public interface OnFragmentInteractionListener {
         void navegarRegistro();
 
     }
+
+
+
+
+
 }
--- ExpoFpApp/app/src/main/java/com/example/expofpapp/SessionActivity.java
+++ ExpoFpApp/app/src/main/java/com/example/expofpapp/SessionActivity.java
@@ -1,17 +1,70 @@
 package com.example.expofpapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.expofpapp.Fragments.LoginFragment;
 import com.example.expofpapp.Fragments.SignUpFragment;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class SessionActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener, SignUpFragment.OnFragmentInteractionListener {
+import java.util.concurrent.Executor;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class SessionActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener, SignUpFragment.OnFragmentInteractionListener, GoogleApiClient.OnConnectionFailedListener {
+
+    private FirebaseAuth mAuth;
+    private SignInButton buttonLoginGoogle;
+    public static final int SIGN_IN_CODE = 777;
+    private GoogleApiClient googleApiClient;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session);
+
+
+        buttonLoginGoogle = findViewById(R.id.buttonLoginGoogle);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("75cdd6b393c9eac9233c4d1c7a2b64cae244e769")
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+/*        buttonLoginGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SessionActivity.this, "HOLA", Toast.LENGTH_SHORT).show();
+                signIn();
+            }
+        });*/
 
         getSupportFragmentManager()
                 .beginTransaction()
@@ -36,5 +89,55 @@ public class SessionActivity extends AppCompatActivity implements LoginFragment.
                 .beginTransaction()
                 .replace(R.id.contenedor, new LoginFragment())
                 .commit();
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, SIGN_IN_CODE);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        Toast.makeText(this, user.getDisplayName(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        /*updateUI(currentUser);*/
+        if (currentUser != null) {
+            updateUI(currentUser);
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(R.id.contenedor_main), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
